d76e47102905bf09dac21ab888d4b13db7143736
I'm ÀŚHÌŚH  MÀŁEY <90534593+Ashishmaley@users.noreply.github.com>
2023-09-10 19:04:40 +0530

    Sign up, Login,Forgot Password activity completed with firebase auth.

--- app/src/main/java/com/example/civilink/MainActivity.kt
+++ app/src/main/java/com/example/civilink/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.civilink
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
